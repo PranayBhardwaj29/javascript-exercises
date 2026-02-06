e9b1b0b21380707ed936db0eabde41cccdbcebef
Pranay Bhardwaj <235537439+PranayBhardwaj29@users.noreply.github.com>
2026-02-06 06:12:49 +0530

    done

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(st, end) {
+    let sum = 0;
+    if (!Number.isInteger(st) || !Number.isInteger(end)) return "ERROR";
+    if(st < 0 || end < 0) {
+        return 'ERROR';
+    }
+    if(st > end) {
+        let temp = st;
+        st = end;
+        end = temp;
+    }
+    for (let index = st; index <= end; index++) {
+        sum = sum + index;        
+    }
+    return sum;
 };
 
 // Do not edit below this line
