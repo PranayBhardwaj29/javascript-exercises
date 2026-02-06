3742ad05e3665fdf6a953ba4ac609676bb826fc5
Pranay Bhardwaj <235537439+PranayBhardwaj29@users.noreply.github.com>
2026-02-06 05:40:12 +0530

    done

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const n = word.length;
+    let temp = "";
+    for (let index = n - 1; index >= 0; index--) {
+        temp += word[index];
+    }
+    return temp;
 };
 
 // Do not edit below this line
