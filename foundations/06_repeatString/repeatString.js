522a2d92102a1819ba86f47762028734f44f01d3
Pranay Bhardwaj <235537439+PranayBhardwaj29@users.noreply.github.com>
2026-02-06 05:31:15 +0530

    done

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (word, times) {
+  if (times < 0) {
+    return "ERROR";
+  }
+  let temp = "";
+  for (let index = 0; index < times; index++) {
+    temp += word;
+  }
+  return temp;
 };
+
+repeatString();
 
 // Do not edit below this line
 module.exports = repeatString;
